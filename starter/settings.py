9a29c257b085aea00462f2dcf82782fb47e5e258
Mikhail <mishnade@gmail.com>
2016-02-29 03:45:01 -0800

    fixing warnings

--- starter/settings.py
+++ starter/settings.py
@@ -23,10 +23,32 @@ SECRET_KEY = 'dh3j1ef_nh2f!t(54_i*84q4wj%aqrib&980!l9s1$hbndmmq2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+# TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 # Application definition
 
@@ -89,12 +111,9 @@ STATICFILES_DIRS = [
     # '/var/www/static/',
 ]
 
-# FIXTURE_DIRS = '/fixtures/'
-#     (
-#    os.path.join(BASE_DIR, 'fixtures'),
-# )
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 
-# MYDATA = get_data()
+
